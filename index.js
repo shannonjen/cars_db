e8792439c8a943ff5786028e4e0bf8037746d9bd
Sam Hoffman <samdhoffman@gmail.com>
2017-10-16 11:57:08 -0400

    added express

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const env = 'development';
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
+
+const express = require('express');
+const app = express();
+
+app.listen(8000, function() {
+  console.log("Listening on port 8000...");
+})
